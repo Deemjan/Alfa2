2009d3142e6829895b5ffe52f0b14ff2f4da94ff
Deemjan <orda1500@gmail.com>
2021-01-03 20:31:40 +0500

    added some hosts

--- Alfa2/settings.py
+++ Alfa2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g+6=9)%^3!86^2x#7zldfxa_k^hp*qp&v@e%dxti4721k03t%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'afternoon-brushlands-89966.herokuapp.com']
 
 
 # Application definition
