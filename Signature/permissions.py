9c564c1920857c5bd26eab848829a7f969cd9a25
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2021-01-05 04:57:53 +0500

    add permission add url for query owner keys add views for query owner keys add new users in db add git ignore my venv refactor KeyTables __str__

---
+++ Signature/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAuthenticatedAndKeyOwner(BasePermission):
+    """
+    The request is authenticated as a user, or is a read-only request.
+    """
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_authenticated and obj.user == request.user
+        )
