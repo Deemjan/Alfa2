4d1ef6577eb2921d576f2b984a7c7fdba94d84b8
Deemjan <orda1500@gmail.com>
2021-01-06 18:18:07 +0500

    added registration/authorization functional

--- Alfa2/settings.py
+++ Alfa2/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'Signature.apps.SignatureConfig',
+    'authentication.apps.AuthConfig',
 ]
 
 MIDDLEWARE = [
--- Signature/urls.py
+++ Signature/urls.py
@@ -7,14 +7,13 @@ from Signature.views import KeyTableViewSet, SignedDocumentViewSet
 router = SimpleRouter()
 router.register(r'keys', KeyTableViewSet)
 router.register(r'docs', SignedDocumentViewSet)
-router.register(r'etKeyOwnerViewSet', views.KeyOwnerViewSet, basename='test')
+router.register(r'getKeyOwnerViewSet', views.KeyOwnerViewSet, basename='test')
 
 urlpatterns = [
     path('uploadCheck', views.uploadCheckView),
     path('uploadHandler', views.VerifyDocumentView.as_view()),
     path('generateKey', views.GenerateKeyView.as_view()),
     path('signDocHandler', views.SignDocumentView.as_view()),
-    # path('etKeyOwnerViewSet', views.KeyOwnerViewSet.as_view())
     #path('', views.openUserPage)
 ]
 
